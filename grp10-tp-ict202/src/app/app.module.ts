5c3fd4e6d67cfad2d17188da0845141125576e60
daniel iya <95070805+FoubaICT4D@users.noreply.github.com>
2022-05-31 08:13:35 +0200

    config firebase

--- grp10-tp-ict202/src/app/app.module.ts
+++ grp10-tp-ict202/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- grp10-tp-ict202/src/environments/environment.ts
+++ grp10-tp-ict202/src/environments/environment.ts
@@ -6,16 +6,18 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCJ-q2wvJ8AjCLiDL2qb2bzdKzBbMGblKk",
-  authDomain: "grp10-tp-ict202-c2bb7.firebaseapp.com",
-  projectId: "grp10-tp-ict202-c2bb7",
-  storageBucket: "grp10-tp-ict202-c2bb7.appspot.com",
-  messagingSenderId: "613011735128",
-  appId: "1:613011735128:web:c526fb117638b97567402d",
-  measurementId: "G-Q025M0E2ZN"
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCJ-q2wvJ8AjCLiDL2qb2bzdKzBbMGblKk",
+    authDomain: "grp10-tp-ict202-c2bb7.firebaseapp.com",
+    projectId: "grp10-tp-ict202-c2bb7",
+    storageBucket: "grp10-tp-ict202-c2bb7.appspot.com",
+    messagingSenderId: "613011735128",
+    appId: "1:613011735128:web:c526fb117638b97567402d",
+    measurementId: "G-Q025M0E2ZN"
+  }
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+
