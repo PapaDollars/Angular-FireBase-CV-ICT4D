9150db0343021d9c3e8f77113e61df12989d06ff
elielemignon <105683680+elielemignon@users.noreply.github.com>
2022-07-05 11:37:37 +0100

    Add files via upload

---
+++ templet1/templet1.component.js
@@ -0,0 +1,24 @@
+
+
+  
+
+function color(){
+    let check= document.getElementById("color");
+    check.style.backgroundColor="red";
+
+}
+
+function color1(){
+    let check1= document.getElementById("color");
+    check1.style.backgroundColor="black";
+}
+
+function color2(){
+    let check2= document.getElementById("color");
+    check2.style.backgroundColor="#2b2a33";
+}
+
+function color3(){
+    let check3= document.getElementById("color");
+    check3.style.backgroundColor="#77514e";
+}
